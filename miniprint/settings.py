a0b84d410e38705788339156e9aa302651be12c0
Fran Hrzenjak <fran@changeset.hr>
2014-12-06 13:43:08 +0100

    project structure

---
+++ miniprint/settings.py
@@ -0,0 +1,93 @@
+"""
+Django settings for miniprint project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Simple but better way to deal with settings_local (without messing about with env)
+try:
+    from . import settings_local
+except ImportError:
+    from . import settings_local_empty as settings_local
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = settings_local.SECRET_KEY
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = settings_local.DEBUG
+
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = settings_local.ALLOWED_HOSTS
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+] + settings_local.ADDITIONAL_INSTALLED_APPS
+
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+] + settings_local.ADDITIONAL_MIDDLEWARE_CLASSES
+
+ROOT_URLCONF = 'miniprint.urls'
+
+WSGI_APPLICATION = 'miniprint.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = settings_local.DATABASES
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media
+
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
