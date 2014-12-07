c5e3214a84cf517fa54601352a60425eac78c7b2
mtroselj <marin.troselj@gmail.com>
2014-12-07 01:45:22 +0100

    - Added templates and views for document and chunks.

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, url
-from .views import HomeView
+from .views import HomeView,DocumentView,ChunkView,LoginView,AboutView
 
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^login/$', AboutView.as_view(), name='about'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^document/(?P<document_id>[\d]+)?$', DocumentView.as_view(), name='document'),
+    url(r'^document/chunk/(?P<chunk_id>[\d]+)?$', ChunkView.as_view(), name='chunk'),
 )
--- web/views.py
+++ web/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'web/index.html'
+
+class DocumentView(TemplateView):
+    template_name = 'web/document.html'
+
+class ChunkView(TemplateView):
+    template_name = 'web/chunk.html'
+
+class AboutView(TemplateView):
+    template_name = 'web/about.html'
+
+class LoginView(TemplateView):
+    template_name = 'web/login.html'
